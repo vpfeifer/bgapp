7964ab4b3ddcf9d30eb1c93d95cf0b1de6c4c671
Vinicius Pfeifer <vpfeifer20@gmail.com>
2022-08-05 22:05:18 -0300

    feat: the blue version

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+const os = require('os')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'THE BACKGROUND COLOR WEB PAGE',
+    version: 'BLUE',
+    hostname: os.hostname()
+  });
+});
+
+module.exports = router;
